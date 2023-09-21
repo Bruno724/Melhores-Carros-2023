52285b1b98bacfdc3d3763d03f136c5fab0fa65c
Bruno724 <brunocoutinho.sp@gmail.com>
2023-09-20 21:56:33 -0300

    javascript

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let  Nome = document.querySelector("#Nome")
+
+const campoIdade = document.querySelector("#Idade")
+const resultado = document.querySelector("#resultado")
+const campoLayoutSim = document.querySelector("#layoutSin")
+const campoLayoutNao = document.querySelector("#layoutNao")
+const campojs = document.querySelector("#js")
+
+
+if (campoLayoutSim.checked) valor += 1  
+if (campoLayoutNao.checked) valor += 1
+if (campojs.checked) valor *= 1.1
+
+
+
